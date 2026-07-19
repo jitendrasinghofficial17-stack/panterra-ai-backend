a7736f439ad85c34a58272349b09c5009f1bee36
Jitendra Singh <jitendrasingh.official17@gmail.com>
2026-07-20 00:38:07 +0530

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -4,11 +4,18 @@ from datetime import datetime, timedelta
 from jose import jwt
 from passlib.context import CryptContext
 
-SECRET_KEY = os.getenv("SECRET_KEY", "change-this-secret-key")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "PanterraSuperSecretKeyChangeThisInProduction"
+)
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 1440
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
 
 
 def hash_password(password: str):
@@ -16,7 +23,10 @@ def hash_password(password: str):
 
 
 def verify_password(password: str, hashed_password: str):
-    return pwd_context.verify(password, hashed_password)
+    return pwd_context.verify(
+        password,
+        hashed_password
+    )
 
 
 def create_access_token(data: dict):
@@ -31,5 +41,5 @@ def create_access_token(data: dict):
     return jwt.encode(
         to_encode,
         SECRET_KEY,
-        algorithm=ALGORITHM,
+        algorithm=ALGORITHM
     )
