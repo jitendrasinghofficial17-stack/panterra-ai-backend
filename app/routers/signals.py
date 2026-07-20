1fcfa2750ca5b8a4d4fef561bd7774077ca18f03
Jitendra Singh <jitendrasingh.official17@gmail.com>
2026-07-20 22:15:47 +0530

    Update signals.py

--- app/routers/signals.py
+++ app/routers/signals.py
@@ -18,7 +18,7 @@ def get_signal(symbol: str):
 
     df = get_historical_data(symbol)
 
-    if df is None:
+    if df is None or df.empty:
         return {
             "status": "error",
             "message": "Market data not available"
@@ -30,6 +30,8 @@ def get_signal(symbol: str):
 
     signal = "HOLD"
     confidence = 55
+    trend = "Sideways"
+    reason = []
 
     if (
         latest["close"] > latest["EMA20"]
@@ -37,8 +39,17 @@ def get_signal(symbol: str):
         and latest["RSI"] < 70
         and latest["MACD"] > latest["MACD_SIGNAL"]
     ):
+
         signal = "BUY"
         confidence = 90
+        trend = "Bullish"
+
+        reason = [
+            "Price above EMA20",
+            "EMA20 above SMA20",
+            "RSI below 70",
+            "MACD Bullish Crossover"
+        ]
 
     elif (
         latest["close"] < latest["EMA20"]
@@ -46,17 +57,35 @@ def get_signal(symbol: str):
         and latest["RSI"] > 30
         and latest["MACD"] < latest["MACD_SIGNAL"]
     ):
+
         signal = "SELL"
         confidence = 90
+        trend = "Bearish"
+
+        reason = [
+            "Price below EMA20",
+            "EMA20 below SMA20",
+            "RSI above 30",
+            "MACD Bearish Crossover"
+        ]
+
+    price = round(float(latest["close"]), 2)
+
+    stop_loss = round(price * 0.98, 2)
+    target = round(price * 1.05, 2)
 
     return {
         "symbol": symbol.upper(),
         "signal": signal,
+        "trend": trend,
         "confidence": confidence,
-        "price": round(float(latest["close"]), 2),
+        "price": price,
+        "target": target,
+        "stop_loss": stop_loss,
         "RSI": round(float(latest["RSI"]), 2),
         "EMA20": round(float(latest["EMA20"]), 2),
         "SMA20": round(float(latest["SMA20"]), 2),
-        "MACD": round(float(latest["MACD"]), 2),
-        "MACD_SIGNAL": round(float(latest["MACD_SIGNAL"]), 2),
+        "MACD": round(float(latest["MACD"]), 4),
+        "MACD_SIGNAL": round(float(latest["MACD_SIGNAL"]), 4),
+        "reason": reason
     }
